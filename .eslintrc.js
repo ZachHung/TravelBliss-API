2f8f7e7f8d683e7bf0dc5f46e23c9374d3d0db27
Thành Hưng <59259431+ZachHung@users.noreply.github.com>
2024-08-04 10:55:14 +0700

    chore: update packages (#6)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,20 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
+    project: ['./tsconfig.json'],
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', ignoreRestSiblings: true },
     ],
-    "no-throw-literal": "error",
-    "@typescript-eslint/explicit-function-return-type": "warn",
+    'no-throw-literal': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
   },
-  ignorePatterns: [
-    "dist/",
-    ".eslintrc.js",
-    "jest.config.ts",
-    "src/migration",
-    "old",
-  ],
+  ignorePatterns: ['dist/', '.eslintrc.js', 'jest.config.ts', 'src/migration', 'old'],
 };
